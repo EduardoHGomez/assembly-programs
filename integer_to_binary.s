ccc5f96d4d408f37d77f3514f07b5b5e123f74e7
Eduardo <hurtado.josegomez@gmail.com>
2024-03-28 20:40:11 -0600

    integer_to binary

---
+++ integer_to_binary.s
@@ -0,0 +1,13 @@
+# Convert an integer to binary
+# Example: 56 to 111000
+# Make it recursive
+
+# Eduardo 28/03/2024
+
+
+.text
+
+main:
+    addi s0 zero 345 # N Input
+    addi s1 zero 0 # Result
+    addi s2 zero 1 # Multiplier
