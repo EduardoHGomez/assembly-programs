d5916e319e58f29d77a05b65c537ee81cf6e04cd
Eduardo <hurtado.josegomez@gmail.com>
2024-04-01 21:10:48 -0600

    Hanoi

--- hanoi.c
+++ hanoi.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 
+void pm(int start, int end) {
+    printf("%d => %d\n", start, end);
+    return;
+}
+
+
+void hanoi(int n, int start, int end) {
+    if (n == 1)
+        pm(start, end);
+    else {
+        int other = 6 - (start + end);
+        hanoi(n-1, start, other);
+        pm(start, end);
+        hanoi(n-1, other, end);
+    }
+
+}
+
+
+
 int main() {
-
-
+    int N = 5;
+    hanoi(N, 1, 3);
     return 0;
 }
+
+
+/*
+def hanoi(n, start, end)
+    if n == 1:
+        pm(start, end)
+    else:
+        other = 6 - (start + end)
+        hanoi(n-1, start, other)
+        pm(start, end)
+        hanoi(n-1, other, end)
+
+
+
+*/
