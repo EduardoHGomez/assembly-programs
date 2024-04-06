ecaaa587f58b5b4804f92df9722e5b375388eb49
Eduardo <hurtado.josegomez@gmail.com>
2024-04-06 00:59:34 -0600

    rework arguments and local variables

--- hanoi.s
+++ hanoi.s
@@ -94,29 +94,28 @@ hanoi: # (N, origen, destino, auxiliar)# Torres de Hanoi 01/04/2024
     addi t0 zero 1 # Asignar a 1
     beq a0 t0 base_case 
 
-    # hanoi(N - 1, origen, auxiliar, destino); (aqui origen se conserva a1=a1, swap entre aux y dest a2 = a3, a3 = a2)
-    # Hacer N - 1
-    addi t0 zero -1 # Asignar - 1 a t0
-    add a0 a0 t0 # N = N - 1
+    # Guardar en variables locales
+    add s8 zero a0 # s8 = N
+    add s9 zero a1 # s9 = ORIGEN
+    add s10 zero a2 # s10 = DESTINO 
+    add s11 zero a3 # s11 = AUXILIAR 
+
+    # === hanoi(N - 1, origen, auxiliar, destino); ====
+    addi a0 s0 -1 # N(argumento) = N - 1
+    add a1 zero s9 # a1 = origen actual (s9)
+    add a2 zero s11 # a2 = auxiliar actual (s11)
+    add a3 zero s10 # a3 = destino actual (s10)
+    jal ra hanoi
     
-    # swap aux y dest -> a2 = a3
-    add s4 zero a2 # s4 = a2
-    add a2 zero a3 # a2 = a3
-    
-    # swap aux y dest -> a3 = a2 (s4)
-    add a3 zero s4
-    
-    jal ra hanoi # hanoi(N-1, origen, auxiliar, destino)
-    
-    # TO DO printf("Mover disco %d desde %c hasta %c\n", N, origen, destino)
-    jal ra swap 
+    # === printf("Mover disco %d desde %c hasta %c\n", N, origen, destino) === 
+    jal ra move
 
-
-
-
-
-
-    # TO DO moverTorres(N - 1, auxiliar, destino, origen);
+    # === moverTorres(N - 1, auxiliar, destino, origen); === 
+    addi a0 s0 -1 # N(argumento) = N - 1
+    add a1 zero s11 # a1 = auxiliar actual (s11)
+    add a2 zero s10 # a2 = destino actual (s10)
+    add a3 zero s9 # a1 = origen actual (s9)
+    jal ra hanoi
 
 
     jal zero end_hanoi_return
@@ -182,7 +181,7 @@ moveOriM3:
 
 base_case:
     # Hacer swap
-    jal ra swap
+    jal ra move
 
     # Ir a end_hanoi 
     jal zero end_hanoi_return
@@ -190,7 +189,7 @@ base_case:
 
 
 
-swap:
+move:
     # t0 = origen
     # t1 = destino 
 
